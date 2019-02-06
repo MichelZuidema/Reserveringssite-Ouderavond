142385e2b063425f9e60a1f865424b8122c4bc95
Sebastian Tramper <tramper.s@outlook.com>
2019-02-06 21:27:28 +0100

    Made some minor changes to the navigation

--- Website/assets/js/script.js
+++ Website/assets/js/script.js
@@ -1,17 +1,27 @@
+
 // simple adventlisteners for the hamburger menu
 var navMenu = document.querySelector("header nav");
 var body = document.querySelector("body");
 
     var hamburger = document.querySelector("section:nth-child(1)").addEventListener("click", function(){
-        navMenu.style.top = "0";
+        navMenu.style.display = "block";
         navMenu.style.width = "100%";
-        navMenu.style.transition = "width 2sec";
         body.style.overflow = "hidden";
     });
 
     var cross = document.querySelector("header nav article").addEventListener("click", function(){
-        navMenu.style.top = "-4000px";
+            navMenu.style.display = "none";
     });
 
+    var link = document.querySelector("header nav ul").addEventListener("click", function(){
+        navMenu.style.display = "none";
+});
 
+//     var ChangeToNormalDesktop = document.querySelector("section:nth-child(1)").addEventListener("resize", function(){
+//         var widthOfScreen = window.innerWidth;
+//         if(widthOfScreen >= 700){
+//         navMenu.style.display = "block";
+//         }
+    
+// });
 
