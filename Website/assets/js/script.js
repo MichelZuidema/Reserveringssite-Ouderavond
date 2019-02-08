5c884f4238387edea84e6059fe54fb671d13241e
S.Tramper <tramper.s@outlook.com>
2019-02-08 10:40:12 +0100

    Made the navigation smaller & added some animations

--- Website/assets/js/script.js
+++ Website/assets/js/script.js
@@ -1,27 +1,16 @@
 
-// simple adventlisteners for the hamburger menu
-var navMenu = document.querySelector("header nav");
-var body = document.querySelector("body");
+$("section:nth-child(1)").click(function(){
+        $("header nav").addClass("toggleOn");
+      });
+$("header nav article").click(function(){
+    $("header nav").removeClass("toggleOn")
+})
 
-    var hamburger = document.querySelector("section:nth-child(1)").addEventListener("click", function(){
-        navMenu.style.display = "block";
-        navMenu.style.width = "100%";
-        body.style.overflow = "hidden";
-    });
-
-    var cross = document.querySelector("header nav article").addEventListener("click", function(){
-            navMenu.style.display = "none";
-    });
-
-    var link = document.querySelector("header nav ul").addEventListener("click", function(){
-        navMenu.style.display = "none";
+$(window).resize(function() {
+  if ($(window).width() > 1023) {
+     $("header nav").removeClass("toggleOn")
+  }
 });
 
-//     var ChangeToNormalDesktop = document.querySelector("section:nth-child(1)").addEventListener("resize", function(){
-//         var widthOfScreen = window.innerWidth;
-//         if(widthOfScreen >= 700){
-//         navMenu.style.display = "block";
-//         }
-    
-// });
+
 
