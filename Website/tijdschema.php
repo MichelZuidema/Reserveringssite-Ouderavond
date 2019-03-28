658ed663bcb699d24616035fd7b8a24d1a29b672
S.Tramper <tramper.s@outlook.com>
2019-03-28 15:47:39 +0100

    checkboxes for michel

--- Website/tijdschema.php
+++ Website/tijdschema.php
@@ -7,22 +7,29 @@
         require 'assets/include/header.php';
     ?>
     <main id="tijdschema">
-        <form action="#" method="POST">
+        <form action="bestaatNogNoiet.php" method="POST" onsubmit="return false">
             <section id="choose-time">
                 <h2>Kies een Datum</h2>
-                <div>
-                    <p>Donderdag</p>
-                    <p>28/04/2019</p>
-                </div>
+
+                    <input type="checkbox">
+                    <section id="checkbox-text">
+                        <p>Donderdag</p>
+                        <p>28/04/2019</p>
+                    </section>
+
                 <p><strong>Click</strong> de gewenste datum aan a.u.b</p>
             </section>
             <article id="timetable">
                 <h2>Tijdschema</h2>
                 <ul>
-                    <li id="time-1"><p>18:00 - 19:00</p></li>
-                    <li id="time-2"><p>19:00 - 20:00</p></li>
-                    <li id="time-3"><p>20:00 - 21:00</p></li>
-                    <li id="time-4"><p>21:00 - 22:00</p></li>
+                    <input type="checkbox" id="time-1">
+                    <p>18:00 - 19:00</p>
+                    <input type="checkbox" id="time-2">
+                    <p>19:00 - 20:00</p>   
+                    <input type="checkbox" id="time-3">
+                    <p>20:00 - 21:00</p>
+                    <input type="checkbox" id="time-4">
+                    <p>21:00 - 22:00</p>
                 </ul>
                 <section id="timetable-info">
                     <section>   
@@ -35,9 +42,9 @@
             <section id="number-of-persons">
                 <h2>Aantal personen</h2>
                 <ul>
-                    <li id="person-1"><p>1 persoon</p></li>
-                    <li id="person-2"><p>2 personen</p></li>
-                    <li id="person-3"><p>3 personen</p></li>
+                    <input type="checkbox" id="person-1"><p>1 persoon</p>
+                    <input type="checkbox" id="person-2"><p>2 personen</p>
+                    <input type="checkbox" id="person-3"><p>3 personen</p>
                 </ul>
                 <p>Selecteer met hoeveel personen u wilt komen</p>
             </section>
@@ -50,8 +57,8 @@
             <article id="ask-question">
                 <h2>Opmerking of vraag</h2>
                 <textarea cols="30" rows="10" placeholder="Hier komt uw vraag/opmerking"></textarea>
-                <input type="" class="sendButton" value="Aanmelding versturen">
-                <input type="" id="showButton" value="Bekijk mijn huidige aanmelding">
+                <input type="submit" class="sendButton" value="Aanmelding versturen">
+                <input type="button" id="showButton" value="Bekijk mijn huidige aanmelding">
             </article>
             <article id="summary">
                 <section>
