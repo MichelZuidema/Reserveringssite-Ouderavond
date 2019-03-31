1227111e508b4fe98fbd9edfb7ddf29975c003a4
S.Tramper <tramper.s@outlook.com>
2019-03-31 16:34:21 +0200

    added mentortijdschema page / not finished

--- Website/index.php
+++ Website/index.php
@@ -14,13 +14,12 @@
             require 'assets/include/header.php';
         }
     ?>
-<<<<<<< HEAD
     <main class="homepage">
             <!-- General information -->
             <article class="information article">
                 <h2 class="information__heading">Algemene informatie</h2>
                 <div class="information__container">
-                    <p class="information__container--text">dat een Lorem ipsum dolor lorem lorem sit amet consectetur, adipisicing elit. Fugit saepe quaerat ut deserunt, non et facilis perspiciatis reiciendis dignissimos, reprehenderit quod consequatur totam. Laboriosam velit adipisci veniam, quod quos harum. lezer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt iste, minima ex magnam tenetur voluptates culpa. Rem, labore expedita eius quibusdam error ipsam corrupti temporibus ab, earum nesciunt blanditiis dicta. tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in uw tegenstelling tot “Hier uw tekst, hier uw tekst” wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina </p>
+                    <p class="information__container--text">dat een Lorem lorem ipsum dolor lorem lorem sit amet consectetur, adipisicing elit. Fugit saepe quaerat ut deserunt, non et facilis perspiciatis reiciendis dignissimos, reprehenderit quod consequatur totam. Laboriosam velit adipisci veniam, quod quos harum. lezer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt iste, minima ex magnam tenetur voluptates culpa. Rem, labore expedita eius quibusdam error ipsam corrupti temporibus ab, earum nesciunt blanditiis dicta. tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in uw tegenstelling tot “Hier uw tekst, hier uw tekst” wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina </p>
                 </div>
             </article>
             <!-- backgroundimage -->
@@ -74,57 +73,6 @@
             </article>
             <!-- background image -->
             <section class="article__image--3"></section>
-=======
-    <main id="homepage">
-            <article>
-                <h2>Algemene informatie</h2>
-                <div>
-                    <p>dat een Lorem ipsum dolor lorem lorem sit amet consectetur, adipisicing elit. Fugit saepe quaerat ut deserunt, non et facilis perspiciatis reiciendis dignissimos, reprehenderit quod consequatur totam. Laboriosam velit adipisci veniam, quod quos harum. lezer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt iste, minima ex magnam tenetur voluptates culpa. Rem, labore expedita eius quibusdam error ipsam corrupti temporibus ab, earum nesciunt blanditiis dicta. tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in uw tegenstelling tot “Hier uw tekst, hier uw tekst” wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina </p>
-                </div>
-            </article>
-            <section id="main-image-1"></section>
-            <article>
-                <h2>Hoe schrijf ik me in</h2>
-                    <ol>
-                        <li>1. quidem asperiores expedita aliquam? In enim voluptas quidem.</li>
-                        <li>2. Natus assumenda quaerat beatae quia a dolorum eligendi</li>
-                        <li>3. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
-                        <li>4. tenetur, perferendis quae quia asperiores aut.</li>
-                    </ol>
-            </article>
-            <!-- layout background images -->
-            <section id="main-image-2"></section>
-            <article id="main-diagram">
-                <h2>Ingeschreven ouders</h2>
-                <section id="main-diagram-grid">
-                    <section class="main-diagram-1">
-                        <p>18:00</p>
-                        <section id="diagram-1"></section>
-                        <h3>60%</h3>
-                        <p>32/33</p>
-                    </section>
-                    <section class="main-diagram-2">
-                        <p>19:00</p>
-                        <section id="diagram-2"></section>
-                        <h3>60%</h3>
-                        <p>32/33</p>
-                    </section>
-                    <section class="main-diagram-3">
-                        <p>20:00</p>
-                        <section id="diagram-3"></section>
-                        <h3>60%</h3>
-                        <p>32/33</p>
-                    </section>
-                    <section class="main-diagram-4">
-                        <p>21:00</p>
-                        <section id="diagram-4"></section>
-                        <h3>60%</h3>
-                        <p>32/33</p>                
-                    </section>
-                </section>
-            </article>
-            <section id="main-image-3"></section>
->>>>>>> 118995e75918c8442b9ab21a47d8c70f047a8176
     </main>
     <?php
         // footer
